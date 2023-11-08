d22277dbbc9b32fd87e8795b4294b296ab7fe960
kamalubd <u.kamal.88@gmail.com>
2023-11-08 23:23:42 +0600

    api category created

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-console.log('a');
+fetch('https://openapi.programming-hero.com/api/news/categories')
+.then(res => res.json())
+.then(data => category(data.data.news_category))
+
+function category (data){
+
+    data.forEach(category => {
+
+        const categoryName = category.category_name;
+
+        const categoryField = document.getElementById('categories');
+
+        const a = document.createElement('a');
+        a.innerHTML = `<a class="nav-link text-white fs-5" href="#">${categoryName}</a>`;
+
+        categoryField.appendChild(a);
+        
+    });
+}
